dbec8cac6fad50275c5159b8b9144ddcf5a55e79
zanattabruno <gkbsa2504@gmail.com>
2023-08-10 19:47:55 +0000

    Update configs

--- build_container_image.sh
+++ build_container_image.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker build . --tag zanattabruno/ric-plt-vespamgr && docker push zanattabruno/ric-plt-vespamgr
+
+kubectl rollout restart deployment deployment-ricplt-vespamgr -n ricplt
