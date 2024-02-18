ec3b3d118e12eb5960c1478d6dca2361f9b73736
zanattabruno <gkbsa2504@gmail.com>
2024-02-18 15:32:28 +0000

    Refactor build_container_image.sh script to allow custom repotag

--- build_container_image.sh
+++ build_container_image.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-docker build . --tag zanattabruno/ric-plt-vespamgr && docker push zanattabruno/ric-plt-vespamgr
+usage="$(basename "$0") [-h] [-r] -- script to build image from helm-cli
 
-kubectl rollout restart deployment deployment-ricplt-vespamgr -n ricplt
+where:
+    -h  show this help text
+    -r  set repotag | default zanattabruno/ric-plt-vespamgr:energy-saver"
+
+repotag=zanattabruno/ric-plt-vespamgr:energy-saver
+
+if [ "$*" == "" ]; then
+    echo "No flag is passed using default repotag $repotag"
+fi
+
+while getopts :hr: flag
+do
+    case "${flag}" in
+        r) repotag=${OPTARG};;
+        h) echo "$usage"
+        exit 0;;
+    esac
+done
+
+docker build . -t $repotag && docker push $repotag 
