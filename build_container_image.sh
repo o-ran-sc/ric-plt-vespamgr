868065577539da1d38c1904ac7719ed0dad11a8b
zanattabruno <gkbsa2504@gmail.com>
2024-05-21 20:49:11 +0000

    chore: Refactor build_container_image.sh script to allow custom repotag

--- build_container_image.sh
+++ build_container_image.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Usage: build_container_image.sh [-h] [-r]
+#        script to build image from helm-cli
+#
+# Options:
+#   -h  Show this help text
+#   -r  Set repotag (default: zanattabruno/ric-plt-vespamgr:energy-saver)
+#
+# Description:
+#   This script builds a Docker image from the helm-cli. It allows you to specify a custom repotag using the -r option, 
+#   otherwise it uses the default repotag. The built image is then pushed to the specified repotag.
+#
+# Example usage:
+#   $ build_container_image.sh -r myrepo/myimage:latest
+#   $ build_container_image.sh -h
+
 usage="$(basename "$0") [-h] [-r] -- script to build image from helm-cli
 
 where:
@@ -21,4 +36,4 @@ do
     esac
 done
 
-docker build . -t $repotag && docker push $repotag 
+docker build . -t $repotag && docker push $repotag
